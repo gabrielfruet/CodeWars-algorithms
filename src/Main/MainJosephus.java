5fb602783df2c82889903d34448eae6c1b8b3558
Gabriel Fruet <gabrielfruet538@gmail.com>
2022-01-31 16:20:25 -0300

    implementing MainJosephus

--- src/Main/MainJosephus.java
+++ src/Main/MainJosephus.java
@@ -1,9 +1,17 @@
 package Main;
 
+import Katas.Josephus;
+
+import java.util.ArrayList;
+
 public class MainJosephus {
 
     public static void main(String[] args) {
-
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        for(int i = 0; i < 20; i++){
+            lista.add(i);
+        }
+        System.out.println(Josephus.josephusPermutation(lista, 7));
     }
 
 }
